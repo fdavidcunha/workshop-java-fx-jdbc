d674d9780a3156ef0176f4e87ff4141509a3e390
David <f.david.cunha@gmail.com>
2021-06-24 07:40:08 -0300

    Criação do itens de menu

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,8 +3,8 @@ package application;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,8 +12,13 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			ScrollPane scroll = loader.load();
+			
+			// Alinhando o scroll box alClient.
+			scroll.setFitToHeight(true);
+			scroll.setFitToWidth(true);
+			
+			Scene mainScene = new Scene(scroll);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
